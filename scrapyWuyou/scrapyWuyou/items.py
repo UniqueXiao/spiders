a88d5b48ae211cb0438c61315812c60e0ad551fd
UniqueXiao <xiaoxin1219_work@163.com>
2018-12-11 18:23:02 +0800

    Update items.py
    use ItemLoader

--- scrapyWuyou/scrapyWuyou/items.py
+++ scrapyWuyou/scrapyWuyou/items.py
@@ -6,15 +6,49 @@
 # https://doc.scrapy.org/en/latest/topics/items.html
 
 import scrapy
+from scrapy.loader.processors import MapCompose
+from scrapy.loader.processors import TakeFirst
 
+
+def lst_convert_str(lst):
+	try:
+		res = ';'.join(lst).strip().replace('\t', '').replace('\r', '').replace('\n', '').replace('\xa0','')
+	except:
+		res = lst
+		
+	return res
 
 class ScrapywuyouItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-	position = scrapy.Field()
-	salary = scrapy.Field()
-	company = scrapy.Field()
-	job_tag = scrapy.Field()
-	job_welfare = scrapy.Field()
-	#job_msg = scrapy.Field()
-	#job_demands = scrapy.Field()
+	# define the fields for your item here like:
+	# name = scrapy.Field()
+	position = scrapy.Field(
+		input_processor = lst_convert_str,
+		output_processor= TakeFirst()
+    )
+	salary = scrapy.Field(
+		input_processor = lst_convert_str,
+		output_processor= TakeFirst()
+	)
+	company = scrapy.Field(
+		input_processor = lst_convert_str,
+		output_processor= TakeFirst()
+	)
+	job_tag = scrapy.Field(
+		input_processor = lst_convert_str,
+		output_processor= TakeFirst()
+	)
+	job_welfare = scrapy.Field(
+		input_processor = lst_convert_str,
+		output_processor= TakeFirst()
+	)
+	job_category = scrapy.Field(
+		input_processor = lst_convert_str,
+		output_processor= TakeFirst()
+	)
+	job_msg = scrapy.Field(
+		input_processor = lst_convert_str,
+		output_processor= TakeFirst()
+	)
+	url = scrapy.Field(
+		output_processor = TakeFirst()
+	)
