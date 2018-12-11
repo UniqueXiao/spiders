74cc7d95b64a72b08b12846658984d171fa55f5c
UniqueXiao <xiaoxin1219_work@163.com>
2018-12-11 18:28:27 +0800

    Update wuyou.py
    use ItemLoader

--- scrapyWuyou/scrapyWuyou/spiders/wuyou.py
+++ scrapyWuyou/scrapyWuyou/spiders/wuyou.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 import scrapy
+from scrapy.loader import ItemLoader
 from scrapyWuyou.items import ScrapywuyouItem
+# from scrapy import log
+# scrapy.log.start(loglevel=ERROR, logstdout=None)
 import time
 from selenium import webdriver
 option = webdriver.ChromeOptions()
@@ -11,10 +14,10 @@ class WuyouSpider(scrapy.Spider):
 	name = 'wuyou'
 	allowed_domains = ['www.51job.com']
 	base_url = 'http://www.51job.com/'
-	visited_page_urls = set()    #存放已爬取过的列表页
-	visited_detail_urls = set()  #存放已爬取过的详情页
+	visited_page_urls = set()    # 存放已爬取过的列表页
+	visited_detail_urls = set()  # 存放已爬取过的详情页
 	#new_urls = set()
-    #start_urls = ['http://www.51job.com/']
+    	#start_urls = ['http://www.51job.com/']
 	
 	def start_requests(self):
 		kwd = self.settings.get('KEYWORDS')[0]   #搜索关键词，定义在settings.py中
@@ -33,6 +36,12 @@ class WuyouSpider(scrapy.Spider):
 		
 
 	def parse(self, response):
+		max_pages = self.settings.get('MAX_PAGES')            # 限制翻页数，定义在settings.py中
+		
+		for link in response.xpath('//div[@class="dw_page"]/div/div/div/ul/li/a/@href').extract():
+			if (link not in self.visited_page_urls) and (len(self.visited_page_urls) < max_pages):           # 翻页链接
+				self.visited_page_urls.add(link)
+				yield scrapy.Request(url=link, callback=self.parse, dont_filter=True)
 		
 		for div in response.xpath('//div[@id="resultList"]/div[contains(@class, "el")]')[1:]:
 			url = div.xpath('p/span/a/@href').extract()[0]    # 详情页链接
@@ -40,18 +49,33 @@ class WuyouSpider(scrapy.Spider):
 				self.visited_detail_urls.add(url)
 				yield scrapy.Request(url=url, callback=self.parse_detail, dont_filter=True)
 			
-		for link in response.xpath('//div[@class="dw_page"]/div/div/div/ul/li/a/@href').extract():
-			if link not in self.visited_page_urls:           # 翻页链接
-				self.visited_page_urls.add(link)
-				yield scrapy.Request(url=link, callback=self.parse, dont_filter=True)
 
 	def parse_detail(self, response):    # 解析详情页
-		item = ScrapywuyouItem()
-		item['position'] = response.xpath('//h1/text()').extract()[0].strip()
-		item['salary'] = response.xpath('//div[@class="cn"]/strong/text()').extract()[0].strip()
-		item['company'] = response.xpath('//div[@class="cn"]/p[@class="cname"]/a[1]/text()').extract()[0].strip()
-		item['job_tag'] = ';'.join(response.xpath('//div[@class="cn"]/p[@class="msg ltype"]/text()').extract()).strip().replace('\xa0', '')
-		item['job_welfare'] = ';'.join(response.xpath('//div[@class="jtag"]/div[@class="t1"]/span/text()').extract())
+		# 使用ItemLoader
+		wuyouItemLoader = ItemLoader(item=ScrapywuyouItem(), response=response)
 		
-		yield item
+		wuyouItemLoader.add_xpath('position', '//h1/text()')
+		wuyouItemLoader.add_xpath('salary', '//div[@class="cn"]/strong/text()')
+		wuyouItemLoader.add_xpath('company', '//div[@class="cn"]/p[@class="cname"]/a[1]/text()')
+		wuyouItemLoader.add_xpath('job_tag', '//div[@class="cn"]/p[@class="msg ltype"]/text()')
+		wuyouItemLoader.add_xpath('job_welfare', '//div[@class="jtag"]/div[@class="t1"]/span/text()')
+		wuyouItemLoader.add_xpath('job_category', '//div[@class="mt10"]/p/a/text()')
+		wuyouItemLoader.add_xpath('job_msg', '//div[@class="bmsg job_msg inbox"]/p/text()')
+		wuyouItemLoader.add_value('url', response.url)
+		
+		# 将提取好的数据load出来
+		yield wuyouItemLoader.load_item()
+		
+		# 直接使用item
+		# item = ScrapywuyouItem()
+		# item['position'] = response.xpath('//h1/text()').extract()[0].strip()
+		# item['salary'] = response.xpath('//div[@class="cn"]/strong/text()').extract()[0].strip()
+		# item['company'] = response.xpath('//div[@class="cn"]/p[@class="cname"]/a[1]/text()').extract()[0].strip()
+		# item['job_tag'] = ';'.join(response.xpath('//div[@class="cn"]/p[@class="msg ltype"]/text()').extract()).strip().replace('\xa0', '')
+		# item['job_welfare'] = ';'.join(response.xpath('//div[@class="jtag"]/div[@class="t1"]/span/text()').extract())
+		# item['job_category'] = ';'.join(reaponse.xpath('//div[@class="mt10"]/p/a/text()').extract()).strip().replace('\t','')
+		# item['job_msg'] = response.xpath('//div[@xlass="bmsg job_msg inbox"]/p/text()').extract()
+		
+		# yield item
+		
 		
